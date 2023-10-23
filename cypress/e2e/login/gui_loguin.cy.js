ecda57e3b30d46265f49a15ed58af3c56052f678
luiscarlos <luiscarlos8pe@gmail.com>
2023-10-23 16:54:02 -0300

    Automação do site https://opensource-demo.orangehrmlive.com/web/index.php/auth/login

--- cypress/e2e/login/gui_loguin.cy.js
+++ cypress/e2e/login/gui_loguin.cy.js
@@ -1,52 +1,86 @@
 /// <reference types="Cypress"/>
 
-describe('Teste Funcional de login', () => {
-    it('Deve realizar o login com sucesso', () => {
+describe('Teste Funcional com Cypress', () => {
+
+
+    it('Deve acessar a pagina do google e verificar se aparece o nome "Aproximadamente"', () => {
+        cy.visit("https://www.google.com/");
+        cy.get('#APjFqb').type('aula de teclado {enter}');
+        cy.contains('Aproximadamente')
+
+    });
+
+    it('Deve realizar o login com sucesso na pagina do Cypress', () => {
         cy.visit("https://example.cypress.io/commands/actions")
         cy.get('#password1')
-        .type('123').click()
-        .should('have.value','123')
-        
-    });
-});
+            .type('123').click()
+            .should('have.value', '123')
 
-describe('Teste Funcional de login', () => { //Funcionalidade 
+    });
+
+    
+    it.only('CT001 Deve acessar a pagina com sucesso', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    });
+
+    it.only('CT002 Deve realizar o login com sucesso', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
+        cy.get('.oxd-button').click();
+    });
+
+    it.only('CT003 Deve acessar o (My Info)', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
+        cy.get('.oxd-button').click();
+        cy.contains('My Info').click();
+        cy.contains('Colonel Sanders');
+    });
+
+    it.only('CT004 Incluindo nickname dentro de (My Info)', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
+        cy.get('.oxd-button').click();
+        cy.contains('My Info').click();
+        cy.contains('Colonel Sanders');
+        cy.get(':nth-child(1) > .oxd-grid-3 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').click().type('Carlos');
+    });
+
+
+
     it('Deve clicar no link e digitar email no campo de email', () => {// Descrição
         cy.visit("https://example.cypress.io")//Visitar o site
         cy.contains('type').click()//Encontra o texto "Type" e clica nele
         cy.url().should('include', '/commands/actions')//verifica se a url acessada apos clicar no type foi incluido (/commands/actions) 
         cy.get('.action-email').type('fake@email.com')//Pegou o campo email 
-        cy.get('.action-email').should('have.value','fake@email.com')//pega o campo email e verifica se o email digitado foi o (fake@email.com)
-        
-        
+        cy.get('.action-email').should('have.value', 'fake@email.com')//pega o campo email e verifica se o email digitado foi o (fake@email.com)
+
+
     });
-});
 
-describe('Teste Funcional de login', () => { //Funcionalidade 
-    it('Deve clicar no link e digitar email no campo de email', () => {// Descrição
-        cy.visit("https://example.cypress.io")//Visitar o site
-        cy.contains('type').click();
-        cy.url().should('include', '/commands/actions');
-        cy.get('.action-email').type('fake@email.com');
-        cy.get('.action-email').should('have.value', 'fake@email.com');
-      
+
+
+
+
+    it('Marca como completo e apaga lista de completos', () => {
+        cy.visit("http://todomvc-app-for-testing.surge.sh/")//Visitar o site
+        cy.get('.new-todo').type('Fazer backup{enter}')
+        cy.get('.toggle').click() // marca como completo
+        cy.contains('Clear completed')// Verifica se tem esse texto 
+        cy.get('.todo-list li').should('have.length', 0)//possui 0 itens
     });
+
+
+    it('incluir  dois itens e conferir o nome do ultimo item e do primeiro item ', () => {
+        cy.visit('https://example.cypress.io/todo');
+        cy.get('.new-todo').type('1 nome {enter}');
+        cy.get('.new-todo').type('2 nome {enter}');
+        cy.get('.todo-list li').first().should('have.text', 'Pay electric bill');
+        cy.get('.todo-list li').last().should('have.text', '2 nome');
+
+    });  
 });
-
-
-it('Marca como completo e apaga lista de completos', () => {
-    cy.visit("http://todomvc-app-for-testing.surge.sh/")//Visitar o site
-    cy.get('.new-todo').type('Fazer backup{enter}')
-    cy.get('.toggle').click() // marca como completo
-    cy.contains('Clear completed')
-    cy.get('.todo-list li').should('have.length',0)//possui 0 itens
-  });
-
-  it.only('Marca como completo e apaga lista de completos', () => {
-    cy.visit("http://todomvc-app-for-testing.surge.sh/")//Visitar o site
-    cy.get('.new-todo').type('Fazer backup{enter}')//pega o campo pela classe e digita o nome e tecla enter ('Fazer backup{enter}')
-    cy.get('.toggle').click() // marca como completo
-    cy.contains('Clear completed').click()// verifica se tem o texto na pagina
-    cy.get('.todo-list li').should('have.length',0)//possui 0 itens
-  });
-
